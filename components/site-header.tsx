3c50742963d11cda6ab0f52eefbf0be26092b610
ivantomicic-crowdhouse <ivan@tomicic.dev>
2026-01-04 13:09:52 +0100

    Update SiteHeader component for improved responsiveness and styling. Adjust SidebarTrigger and Separator visibility for mobile and desktop views. Enhance title styling for better readability across different screen sizes.

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -40,12 +40,14 @@ export function SiteHeader({
 	return (
 		<header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-16 flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
 			<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-				<SidebarTrigger className="-ml-1" />
+				<SidebarTrigger className="-ml-1 hidden md:block" />
 				<Separator
 					orientation="vertical"
-					className="mx-2 data-[orientation=vertical]:h-4"
+					className="mx-2 hidden md:block data-[orientation=vertical]:h-4"
 				/>
-				<h1 className="text-base font-medium">{title}</h1>
+				<h1 className="text-xl font-heading font-semibold md:text-base md:font-medium">
+					{title}
+				</h1>
 				<div className="ml-auto flex items-center gap-2">
 					{showDefaultAction ? (
 						<Button asChild size="sm">
